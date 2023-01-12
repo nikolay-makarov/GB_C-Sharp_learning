63bb4abff93f41091372d0c4e1d39d9a1131c90d
Nicolay Makarov <i.n.makarov@mail.ru>
2023-01-13 01:42:06 +0500

    add Example005 condition if-else

---
+++ Example005_condition_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string name = Console.ReadLine();
+
+if(name.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.WriteLine("Здравствуй " + name);
+}
