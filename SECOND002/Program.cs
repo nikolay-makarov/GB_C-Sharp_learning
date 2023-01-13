b1b22d83a4c197fa00212f33b5a24896d9ad2bc6
Nicolay Makarov <i.n.makarov@mail.ru>
2023-01-14 00:25:10 +0500

    Add some seminar's exercises

---
+++ SECOND002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+string number1Str = Console.ReadLine();
+
+Console.WriteLine("Введите второе число: ");
+string number2Str = Console.ReadLine();
+
+int number1 = Convert.ToInt32(number1Str);
+int number2 = Convert.ToInt32(number2Str);
+if(number1 == number2 * number2)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
